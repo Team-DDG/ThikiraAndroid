28d8329eac481380946af97eec85c6953a8b4cab
kimdohun0104 <dikolight203@gmail.com>
2020-01-11 20:42:41 +0900

    [UI] login xml 작성

---
+++ restaurant/src/main/java/com/dsm/restaurant/Register3Fragment.kt
@@ -0,0 +1,8 @@
+package com.dsm.restaurant
+
+import com.dsm.baseapp.BaseFragment
+
+class Register3Fragment : BaseFragment() {
+    override val layoutResId: Int = R.layout.fragment_register3
+
+}
---
+++ restaurant/src/main/java/com/dsm/restaurant/Register4Fragment.kt
@@ -0,0 +1,8 @@
+package com.dsm.restaurant
+
+import com.dsm.baseapp.BaseFragment
+
+class Register4Fragment : BaseFragment() {
+    override val layoutResId: Int = R.layout.fragment_register4
+
+}
