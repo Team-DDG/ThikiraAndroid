d6a0bb3c0128bb640bfac240f35a4b9cd4b5f623
kimdohun0104 <dikolight203@gmail.com>
2020-01-12 14:46:10 +0900

    [UI] 업체 등록 UI 작성

--- restaurant/src/main/java/com/dsm/restaurant/Register4Fragment.kt
+++ restaurant/src/main/java/com/dsm/restaurant/Register4Fragment.kt
@@ -1,8 +1,21 @@
 package com.dsm.restaurant
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.dsm.baseapp.BaseFragment
+import kotlinx.android.synthetic.main.fragment_register4.*
 
 class Register4Fragment : BaseFragment() {
     override val layoutResId: Int = R.layout.fragment_register4
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tb_register4.setNavigationOnClickListener { findNavController().popBackStack() }
+
+        btn_register.setOnClickListener {
+            findNavController().popBackStack(R.id.loginFragment, true)
+        }
+    }
 }
