324e626827a67ba9cfb1e71db8626578e304b25e
ParkYoungJin0303 <dev.parkyoungjin@gmail.com>
2020-01-17 20:27:35 +0900

    [UPDATE] Splash UI

---
+++ user/src/main/java/com/dsm/thikiraandroid/SplashFragment.kt
@@ -0,0 +1,9 @@
+package com.dsm.thikiraandroid
+
+import com.dsm.baseapp.BaseFragment
+
+class SplashFragment: BaseFragment() {
+
+    override val layoutResId: Int
+        get() = R.layout.fragment_splash
+}
