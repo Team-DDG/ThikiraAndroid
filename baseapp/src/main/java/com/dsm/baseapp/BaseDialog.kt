14f9510067882231c59532dfddea532e2b2dddf2
kimdohun0104 <dikolight203@gmail.com>
2020-01-17 14:06:57 +0900

    [UI] Coupon

--- baseapp/src/main/java/com/dsm/baseapp/BaseDialog.kt
+++ baseapp/src/main/java/com/dsm/baseapp/BaseDialog.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 
 abstract class BaseDialog : DialogFragment() {
@@ -11,4 +12,12 @@ abstract class BaseDialog : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(layoutResId, container, false)
+
+    override fun onResume() {
+        super.onResume()
+        val params: WindowManager.LayoutParams = dialog!!.window!!.attributes
+        params.width = WindowManager.LayoutParams.MATCH_PARENT
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.attributes = params
+    }
 }
