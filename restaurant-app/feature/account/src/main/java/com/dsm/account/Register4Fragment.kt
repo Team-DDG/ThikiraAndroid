77451aed2c742b10b9720a9afd21c9810b69a8da
kimdohun0104 <dikolight203@gmail.com>
2020-02-29 19:19:11 +0900

    [REFAC] register complete

--- restaurant-app/feature/account/src/main/java/com/dsm/account/Register4Fragment.kt
+++ restaurant-app/feature/account/src/main/java/com/dsm/account/Register4Fragment.kt
@@ -1,9 +1,25 @@
 package com.dsm.account
 
+import android.os.Bundle
+import android.view.View
 import com.dsm.account.databinding.FragmentRegister4Binding
+import com.dsm.account.viewModel.RegisterViewModel
 import com.dsm.androidcomponent.base.BaseFragment
+import com.dsm.androidcomponent.ext.setupNavigateEvent
+import com.dsm.androidcomponent.ext.setupToastEvent
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class Register4Fragment : BaseFragment<FragmentRegister4Binding>() {
     override val layoutResId: Int = R.layout.fragment_register4
 
+    private val viewModel: RegisterViewModel by sharedViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupToastEvent(viewModel.toastEvent)
+        setupNavigateEvent(viewModel.navigateEvent)
+
+        binding.viewModel = viewModel
+    }
 }
