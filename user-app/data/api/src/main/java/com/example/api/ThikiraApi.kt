1c9ee3efe8a3e8e643ca0fca2c7a250fd582b044
seung min <3235tmdals@gmail.com>
2020-05-27 23:13:11 +0900

    THIK-21 User App Login / Sign up [Add] added retrofit api interface

---
+++ user-app/data/api/src/main/java/com/example/api/ThikiraApi.kt
@@ -0,0 +1,40 @@
+package com.example.api
+
+import com.example.api.response.TokenResponse
+import com.example.model.User
+import retrofit2.http.*
+
+interface ThikiraApi {
+    @POST("create")
+    suspend fun register(@Body body: User)
+
+    @DELETE("leave")
+    suspend fun leaveThikira(@Header("Authorization") token: String)
+
+    @PATCH("password")
+    suspend fun modifyPassword(
+        @Header("Authorization") token: String,
+        @Body password: String
+    )
+
+    /**
+     * auth
+     */
+    @GET("auth")
+    suspend fun confirmAccessToken(@Header("Authorization") token: String)
+
+    @GET("auth/email")
+    suspend fun confirmEmail(@Query("email") email: String)
+
+    @POST("auth/password")
+    suspend fun confirmPassword(
+        @Header("Authorization") token: String,
+        @Body password: String
+    )
+
+    @POST("auth/sing_in")
+    suspend fun login(@Body body: User): TokenResponse
+
+    @GET("auth/refresh")
+    suspend fun refreshToken(@Header("X--Refresh-Token") token: String): TokenResponse
+}
