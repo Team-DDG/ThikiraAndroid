073e6920eb212d569e0c4055cf1fd70f5928dda0
seung min <3235tmdals@gmail.com>
2020-06-01 16:22:06 +0900

    THIK-32 Add user remote data source and impl [Add] remote data source 추가, Thikira api 수정

--- user-app/data/api/src/main/java/com/example/api/ThikiraApi.kt
+++ user-app/data/api/src/main/java/com/example/api/ThikiraApi.kt
@@ -1,37 +1,45 @@
 package com.example.api
 
 import com.example.api.response.TokenResponse
+import com.example.api.response.UserResponse
 import com.example.model.User
 import retrofit2.http.*
 
 interface ThikiraApi {
+    @GET("/")
+    suspend fun getUserInfo(): UserResponse
+
     @POST("create")
-    suspend fun register(@Body body: User)
+    suspend fun register(@Body body: Any)
 
     @DELETE("leave")
-    suspend fun leaveThikira(@Header("Authorization") token: String)
+    suspend fun leaveThikira()
 
     @PATCH("password")
-    suspend fun changePassword(
-        @Header("Authorization") token: String,
-        @Body password: String
+    suspend fun changePassword(@Body password: String)
+
+    @FormUrlEncoded
+    @PATCH("address")
+    suspend fun changeAddress(
+        @Field("add_parcel") address: String,
+        @Field("add_street") roadAddress: String
     )
 
     /**
      * auth
      */
     @GET("auth")
-    suspend fun confirmAccessToken(@Header("Authorization") token: String)
+    suspend fun confirmAccessToken()
 
     @GET("auth/email")
     suspend fun confirmEmail(@Query("email") email: String)
 
     @POST("auth/password")
-    suspend fun confirmPassword(
-        @Header("Authorization") token: String,
-        @Body password: String
-    )
+    suspend fun confirmPassword(@Body password: String)
+
+    @GET("auth/refresh")
+    suspend fun refreshToken(): TokenResponse
 
     @POST("auth/sing_in")
-    suspend fun login(@Body body: User): TokenResponse
+    suspend fun login(@Body body: Any): TokenResponse
 }
