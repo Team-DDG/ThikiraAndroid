4a2f2ac51434b9f1ea25852af1c50f777f8c4303
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-06-14 10:59:19 +0900

    [THIK-25] [Add] Change Restaurant class & Change Camel Case

--- user-app/feature/main/src/main/java/com/dsm/main/MainViewModel.kt
+++ user-app/feature/main/src/main/java/com/dsm/main/MainViewModel.kt
@@ -13,17 +13,17 @@ class MainViewModel(
 ) : ViewModel() {
     val eventList: MutableLiveData<List<Event>> = MutableLiveData(listOf())
     val restaurantList: MutableLiveData<List<Restaurant>> = MutableLiveData(listOf())
-    private val restaurantMap: HashMap<String, List<Restaurant>?> = hashMapOf(
-        Pair("한식", null),
-        Pair("중식", null),
-        Pair("일식", null),
-        Pair("분식", null),
-        Pair("치킨", null),
-        Pair("피자", null),
-        Pair("야식", null),
-        Pair("도시락", null),
-        Pair("족발 보쌈", null),
-        Pair("패스트푸드", null)
+    private val restaurantMap: HashMap<String, List<Restaurant>> = hashMapOf(
+        Pair("한식", listOf()),
+        Pair("중식", listOf()),
+        Pair("일식", listOf()),
+        Pair("분식", listOf()),
+        Pair("치킨", listOf()),
+        Pair("피자", listOf()),
+        Pair("야식", listOf()),
+        Pair("도시락", listOf()),
+        Pair("족발 보쌈", listOf()),
+        Pair("패스트푸드", listOf())
     )
 
     val progressBarVisibility = MutableLiveData(false)
@@ -38,7 +38,7 @@ class MainViewModel(
 
     fun getRestaurantList(category: String) = viewModelScope.launch {
         progressBarVisibility.value = true
-        if (restaurantMap[category]!!.isNullOrEmpty()) {
+        if (restaurantMap[category]!!.isNotEmpty()) {
             restaurantList.value = restaurantMap[category]
         } else {
             restaurantList.value = restaurantRepository.getRestaurantList("NEARNESS", category)
