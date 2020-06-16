1cc542bdb7c109f3933808c709ab91f373df75a7
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-06-17 02:51:03 +0900

    [THIK-25] [Set] Set google-services.json in .gitignore

--- user-app/base/src/main/java/com/example/base/App.kt
+++ user-app/base/src/main/java/com/example/base/App.kt
@@ -20,7 +20,7 @@ class App : Application() {
                     apiModule,
                     repositoryModule,
                     accountModule,
-                    mainModule
+                    mainModule,
                     firebaseModule
                 )
             )
