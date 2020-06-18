9461f8c53298b22c3b8a7dcfdb4fd3cc7f09303d
KRMKGOLD <dkqk0124@dsm.hs.kr>
2020-06-18 21:52:00 +0900

    [THIK-25] [Modify] Modify Package Name, Add google-services.json in gitignore

--- user-app/feature/main/src/main/java/com/dsm/main/di/mainModule.kt
+++
@@ -1,9 +0,0 @@
-package com.dsm.main.di
-
-import com.dsm.main.MainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val mainModule  = module {
-    viewModel { MainViewModel(get(), get()) }
-}
