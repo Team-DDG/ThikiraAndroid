3d44d90d167d482b45e895e8eb349b594e33dc49
seung min <3235tmdals@gmail.com>
2020-07-16 10:40:48 +0900

    THIK-53 User Add Order(Choice) Module and add Activities [Add] added menu repository implement

---
+++ user-app/data/repository/src/main/java/com/example/repository/MenuRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.example.repository
+
+import com.example.api.datasource.RemoteMenuDataSource
+import com.example.model.Menu
+import com.example.model.MenuCategory
+import com.example.model.repository.MenuRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class MenuRepositoryImpl (
+    private val remoteMenuDataSource: RemoteMenuDataSource,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+): MenuRepository {
+    override suspend fun getMenuList(categoryId: String): List<Menu> = withContext(ioDispatcher) {
+        remoteMenuDataSource.getMenuList(categoryId)
+    }
+
+    override suspend fun getMenuCategory(restaurantId: String): List<MenuCategory> = withContext(ioDispatcher) {
+        remoteMenuDataSource.getMenuCategory(restaurantId)
+    }
+}
