c9b586cacd6b15c23fd22818e6cf05f230e1d4b5
seung min <3235tmdals@gmail.com>
2020-08-24 13:45:40 +0900

    THIK-53 User Add Order(Choice) Module and add Activities [Add] added item for restaurant's menu

--- user-app/feature/restaurant/src/main/java/com/example/restaurant/adapter/RestaurantMenuAdapter.kt
+++ user-app/feature/restaurant/src/main/java/com/example/restaurant/adapter/RestaurantMenuAdapter.kt
@@ -3,6 +3,7 @@ package com.example.restaurant.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.model.Menu
 import com.example.restaurant.databinding.ItemMenuBinding
 import com.example.restaurant.viewmodel.RestaurantViewModel
@@ -21,13 +22,15 @@ class RestaurantMenuAdapter(
 
     override fun getItemCount(): Int = menus.size
 
-    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) = holder.bind()
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) = holder.bind(menus[position])
 }
 
 class MenuViewHolder(private val binding: ItemMenuBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind() {
+    fun bind(item: Menu) {
         binding.root.setOnClickListener {
             //TODO add activity or dialog for menu
         }
+        Glide.with(itemView.context).load(item.image).into(binding.ivMenuImage)
+        binding.tvMenuName.text = item.name
     }
 }
